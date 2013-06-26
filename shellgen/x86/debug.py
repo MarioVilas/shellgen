238a6e1adb907ef658a77e26b34c43aada01e37c
Mario Vilas <mvilas@gmail.com>
2013-06-26 14:10:13 +0200

    Added crasher shellcodes for x86/x86_64.

--- shellgen/x86/debug.py
+++ shellgen/x86/debug.py
@@ -26,14 +26,14 @@
 from __future__ import absolute_import
 from ..base import Dynamic, Static
 
-__all__ = ["Breakpoint", "While1"]
+__all__ = ["Breakpoint", "While1", "Segfault", "StackOverflow"]
 
 #-----------------------------------------------------------------------------#
 
 class Breakpoint (Dynamic):
     "Trigger a software breakpoint interruption."
 
-    qualities = "stack_balanced"
+    qualities = "no_stack"
     encoding  = "nullfree"
 
     def __init__(self, size = 1):
@@ -45,11 +45,34 @@ class Breakpoint (Dynamic):
 class While1 (Static):
     "Infinite loop."
 
-    qualities = "stack_balanced"
+    qualities = "no_stack, payload" # execution stops here
     encoding  = "nullfree"
 
-    bytes  = "\xeb\xfe"  # jmp short $-2
-    length = 2
+    bytes  = "\xeb\xfe" # jmp short $-2
+    length = len(bytes)
+
+class Segfault (Static):
+    "Trigger a segmentation fault."
+
+    qualities = "no_stack, payload" # execution stops here
+    encoding  = "nullfree"
+
+    bytes  = "\x31\xdb\x8b\x1b" # xor ebx, ebx / mov ebx, [ebx]
+    length = len(bytes)
+
+class StackOverflow (Static):
+    """
+    Crash the process by exhausting its stack space.
+
+    On Windows this causes the process to die silently, without showing the
+    application error message.
+    """
+
+    qualities = "payload"           # execution stops here
+    encoding  = "nullfree"
+
+    bytes  = "\x50\xeb\xfd" # push eax / jmp short $-3
+    length = len(bytes)
 
 #-----------------------------------------------------------------------------#
 
@@ -59,7 +82,18 @@ def test():
     assert Breakpoint.os == "any"
     assert While1.arch == "x86"
     assert While1.os == "any"
+    assert Segfault.arch == "x86"
+    assert Segfault.os == "any"
+    assert StackOverflow.arch == "x86"
+    assert StackOverflow.os == "any"
     assert While1().bytes == While1.bytes
     assert While1().length == While1.length
+    assert len(While1().bytes) == While1().length
+    assert Segfault().bytes == Segfault.bytes
+    assert Segfault().length == Segfault.length
+    assert len(Segfault().bytes) == Segfault().length
+    assert StackOverflow().bytes == StackOverflow.bytes
+    assert StackOverflow().length == StackOverflow.length
+    assert len(StackOverflow().bytes) == StackOverflow().length
     assert Breakpoint(0x100).length == 0x100
     assert Breakpoint(0x100).bytes == Breakpoint(1).bytes * 0x100
